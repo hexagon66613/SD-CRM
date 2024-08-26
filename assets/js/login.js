15108c9a9ce75bfa41453b72b3a4696daa8edaea
hexagon66613 <reynaldo.oktavianus@seven-retail.com>
2024-08-26 11:42:21 +0700

    Update login.js

--- assets/js/login.js
+++ assets/js/login.js
@@ -3,13 +3,12 @@ import { getAuth, signInWithEmailAndPassword } from "https://www.gstatic.com/fir
 
 // Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBCidslTYtvEynEeZ9p46UuV5phZ8sliHk",
-  authDomain: "sd-crm-4e151.firebaseapp.com",
-  projectId: "sd-crm-4e151",
-  storageBucket: "sd-crm-4e151.appspot.com",
-  messagingSenderId: "346515488213",
-  appId: "1:346515488213:web:e77b36da1732be1fb3515c",
-  measurementId: "G-5XTC1W70QF"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT_ID.firebaseapp.com",
+  projectId: "YOUR_PROJECT_ID",
+  storageBucket: "YOUR_PROJECT_ID.appspot.com",
+  messagingSenderId: "YOUR_MESSAGING_SENDER_ID",
+  appId: "YOUR_APP_ID"
 };
 
 const app = initializeApp(firebaseConfig);
