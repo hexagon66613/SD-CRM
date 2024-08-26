d05056e4abf831b86a94755690115bda83e380ad
hexagon66613 <reynaldo.oktavianus@seven-retail.com>
2024-08-26 10:25:16 +0700

    Update auth-check.js

--- assets/js/auth-check.js
+++ assets/js/auth-check.js
@@ -1,7 +1,9 @@
 // assets/js/auth-check.js
 
 document.addEventListener('DOMContentLoaded', () => {
+  // Check if 'authenticated' flag is set in localStorage
   if (localStorage.getItem('authenticated') !== 'true') {
+    // Redirect to login page if not authenticated
     window.location.href = 'login.html';
   }
 });
